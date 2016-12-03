70f7ff9991137114191a4a097592cc5e72d4ece3
Gergely Imreh <imrehg@gmail.com>
2016-12-03 14:58:18 +0000

    s & v

--- rainbow.py
+++ rainbow.py
@@ -2,6 +2,7 @@
 
 import colorsys
 import time
+import os
 
 from blinkt import set_clear_on_exit, set_brightness, set_pixel, show
 
@@ -12,12 +13,15 @@ hue = 0
 set_clear_on_exit()
 set_brightness(0.1)
 
+S = float(os.getenv('SVAL', '1.0'))
+V = float(os.getenv('VVAL', '1.0'))
+
 while True:
     hue = int(time.time() * 100) % 360
     for x in range(8):
         offset = x * spacing
         h = ((hue + offset) % 360) / 360.0
-        r, g, b = [int(c*255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
+        r, g, b = [int(c*255) for c in colorsys.hsv_to_rgb(h, S, V)]
         set_pixel(x,r,g,b)
     show()
     time.sleep(0.001)
